aba3244a87da89d526bd1fbafaf33c6fd3998e39
ktleary <kevin@ktleary.com>
2024-01-08 17:12:39 -0500

    return search results from google

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,6 +4,7 @@ import { Form, useLoaderData } from '@remix-run/react';
 import { z } from 'zod';
 import { zx } from 'zodix';
 import styles from '~/styles/main.module.css';
+import { SearchResult, searchGoogle } from '../services/serpapi';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Dexa Coding Interview' }];
@@ -13,17 +14,22 @@ export async function loader(args: LoaderFunctionArgs) {
   const { q } = zx.parseQuery(args.request, {
     q: z.string().optional(),
   });
-  const searchResults: unknown[] = [];
+
+  if (!q) {
+    return json({ q, searchResults: [], summary: '' });
+  }
+  const searchResults: SearchResult[] = await searchGoogle(q);
   const summary = q?.length ? `TODO: Summary of search results for "${q}"` : '';
   return json({ q, searchResults, summary });
 }
 
 export default function Index() {
   const { q, searchResults, summary } = useLoaderData<typeof loader>();
+
   return (
     <div className={styles.main}>
       <h1 className={styles.title}>Puzzlement</h1>
-      <h2 className={styles.subtitle}>The journey starts here</h2>
+      <h2 className={styles.subtitle}>The solution starts here</h2>
       <div className={styles.queryContainer}>
         <Form method="get">
           <input
@@ -38,6 +44,13 @@ export default function Index() {
         </Form>
       </div>
       {summary ? <p>{`Summary: ${summary}`}</p> : null}
+      <ul>
+        {searchResults.map((result, i) => (
+          <li key={i}>
+            <a href={result?.link}>{result?.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- app/services/serpapi.ts
+++ app/services/serpapi.ts
@@ -1,16 +1,53 @@
 import { getJson } from 'serpapi';
 import { EnvVars } from './env-vars';
 
+const API_KEY = EnvVars.serpapi();
+
 /**
  * Use this to get the Google search results for a query.
  * Docs: https://github.com/serpapi/serpapi-javascript
  */
 
-// @TODO: Add a type for the search result.
-export type SearchResult = unknown;
+/*
+{
+      position: 1,
+      title: "and Bob's your uncle Definition & Meaning",
+      link: 'https://www.merriam-webster.com/dictionary/and%20Bob%27s%20your%20uncle',
+      redirect_link: 'https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.merriam-webster.com/dictionary/and%2520Bob%2527s%2520your%2520uncle&ved=2ahUKEwj-mIii3s6DAxUkEFkFHehqBF4QFnoECDIQAQ',
+      displayed_link: 'https://www.merriam-webster.com › dictionary › and ...',
+      favicon: 'https://serpapi.com/searches/659c6933ca968f911d50379c/images/c9eda006c80988eae31d9e33e99bd57302654086032acdf012b2b7af2f63081b.png',
+      snippet: "The meaning of AND BOB'S YOUR UNCLE is —used to say that something is easy to do or use. How to use and Bob's your uncle in a sentence.",
+      snippet_highlighted_words: [Array],
+      source: 'Merriam-Webster'
+    },
+    */
 
-/** Search Google for the given query using the SerpApi service. */
+// @TODO: Add a type for the search result.
+export type SearchResult = {
+  position: number;
+  title: string;
+  link: string;
+  snippet: string;
+  source: string;
+};
+
 export async function searchGoogle(query: string): Promise<SearchResult[]> {
-  // @TODO: Use the SerpApi SDK to perform a Google search.
-  return [];
+  const results = await getJson(
+    {
+      engine: 'google',
+      api_key: API_KEY,
+      q: query,
+      location: 'New York,New York,United States',
+    },
+    (json) => {
+      // console.log(json['organic_results']);
+      // console.log('results received');
+      // return json['organic_results'];
+      return json;
+    }
+  );
+
+  const { organic_results } = results;
+
+  return organic_results;
 }
